1892afe93a01e56ab6edcb9f62d765cb4bcdade8
kusuri12-09 <waterfail12@naver.com>
2025-04-03 20:14:41 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C11/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n-1; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}

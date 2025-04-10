828c8ebc434ae63b37b90add31fae9a5ad2c0935
kusuri12-09 <waterfail12@naver.com>
2025-04-10 15:59:41 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C11/백준/Bronze/2675. 문자열 반복/문자열 반복.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int t;
+	int r;
+	char s[20];
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		scanf("%d %s", &r, s);
+
+		int j = 0;
+		while (s[j]) {
+			for (int k = 0; k < r; k++) {
+				printf("%c", s[j]);
+			}
+			j++;
+		}
+		printf("\n");
+	}
+	return 0;
+}

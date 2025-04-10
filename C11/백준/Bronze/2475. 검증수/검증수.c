e5c536df459753d0a913c821274b8c5ca4fa05a5
kusuri12-09 <waterfail12@naver.com>
2025-04-10 10:28:51 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C11/백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, c, d, e;
+	scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+	printf("%d", (a * a + b * b + c * c + d * d + e * e) % 10);
+
+	return 0;
+}

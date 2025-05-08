3595c62054479e0dc57c72632e34b014e00cd9d4
kusuri12 <waterfail12@naver.com>
2025-05-08 17:34:41 +0900

    [Bronze III] Title: 최댓값, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- Java/백준/Bronze/2562. 최댓값/최댓값.java
+++ Java/백준/Bronze/2562. 최댓값/최댓값.java
@@ -5,16 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int big = 0, big_num = 0;
-        int[] numbers = new int[9];
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (big < numbers[i]) {
-                big = numbers[i];
-                big_num = i + 1;
+        for (int i = 1; i <= 9; i++) {
+            int num = sc.nextInt();
+            if (big < num) {
+                big = num;
+                big_num = i;
             }
         }
 

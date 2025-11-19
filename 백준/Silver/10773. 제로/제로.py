e84c59556cdd9eb23c24bf4cde2a96dd561bf357
kusuri12 <waterfail12@naver.com>
2025-11-20 08:48:59 +0900

    [Silver IV] Title: 제로, Time: 100 ms, Memory: 114580 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+arr = []
+
+for i in range(k):
+    n = int(input())
+    if n == 0:
+        arr.pop()
+    else:
+        arr.append(n)
+
+print(sum(arr))

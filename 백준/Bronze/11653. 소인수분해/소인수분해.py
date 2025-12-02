af6df0a41d137beda659fafcb8ed9f5df2796c71
kusuri12 <waterfail12@naver.com>
2025-12-02 19:49:15 +0900

    [Bronze I] Title: 소인수분해, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,17 +1,13 @@
 n = int(input())
-
-prime = [True] * (n+1)
-prime[0] = prime[1] = False
-
-for i in range(2, int(n**0.5)+1):
-    if prime[i]:
-        for j in range(i*i, n+1, i):
-            prime[j] = False
-prime_numbers = [num for num in range(n+1) if prime[num] and n % num == 0]
-
 res = []
-for p in prime_numbers:
-    while n % p == 0:
-        res.append(p)
-        n //= p
+i = 2
+while i*i <= n:
+    if n % i == 0:
+        res.append(i)
+        n //= i
+    else:
+        i += 1
+if n > 1:
+    res.append(n)
+
 print('\n'.join(map(str, res)))

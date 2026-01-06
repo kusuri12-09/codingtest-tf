76020db0da8179b8a5f31320d53ada469dd076b7
kusuri12 <waterfail12@naver.com>
2026-01-06 19:12:00 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,10 @@
+data = {
+    "Algorithm": "204",
+    "DataAnalysis": "207",
+    "ArtificialIntelligence": "302",
+    "CyberSecurity": "B101",
+    "Network": "303",
+    "Startup": "501",
+    "TestStrategy": "105" }
+for _ in range(int(input())):
+    print(data.get(input()))
